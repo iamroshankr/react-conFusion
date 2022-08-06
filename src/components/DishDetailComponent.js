c3b64e67d66e0f0ee08b1e06d76ad76ebd4d7728
Roshan Kumar <roshanmaurya729@gmail.com>
2022-08-06 21:04:03 +0530

    Integrated Redux Thunk and logger

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Row, Label } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import Loading from './LoadingComponent';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => (val) && (val.length >= len);
@@ -119,7 +120,25 @@ function DishDetail(props) {
     let selectedDish = props.dish;
     let selectedComments = props.comments;
 
-    if(selectedDish !== null) {
+    if(props.isLoading) {
+        return (
+            <div className='conatainer'>
+                <div className='row'>
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if(props.errMess) {
+        return (
+            <div className='conatainer'>
+                <div className='row'>
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if(selectedDish !== null) {
         return (
             <div className='container'>
                 <div className='row'>
